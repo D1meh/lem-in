e6234bad4e8ec75767f0cf19fb81a6c89a7f38aa
Edracoon <77042040+Edracoon@users.noreply.github.com>
2022-05-23 18:20:07 +0200

    heyo

--- src/main.c
+++ src/main.c
@@ -6,42 +6,52 @@
 /*   By: epfennig <epfennig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 15:05:00 by epfennig          #+#    #+#             */
-/*   Updated: 2022/05/23 17:21:38 by epfennig         ###   ########.fr       */
+/*   Updated: 2022/05/23 18:15:34 by epfennig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
 
-int    registerLine(t_data *anthill, char *line) {
-    (void)anthill;
+int    registerLine(t_data *anthill, char *line, unsigned int index) {
     printf("-> |%s|\n", line);
+    if (index == 0)
+    {
+        int nbAnts = ft_atoi(line);
+        if (nbAnts <= 0)
+            return (-1);
+        anthill->ants = malloc(sizeof(t_ant) * nbAnts);
+        anthill->nbAnts = nbAnts;
+    }
+    else if (index > 0)
+    {
+        
+    }
     return 1;
 }
 
 void    readInput(t_data *anthill, int fd) {
-
-    // char    *buff = malloc(sizeof(char) * 0);
-    char    *line;
-    // char    *tmp = NULL;
-
-    // size_t  r = 0;
-    while (42)
-    {    
-        if (get_next_line(fd, &line) < 1) {
-            registerLine(anthill, line);
-            break ;
-        }
-        registerLine(anthill, line);
+    char            *line = NULL;
+    char            *prevLine;
+    unsigned int    index = 0;
+    while (1)
+    {
+        prevLine = line;
+        if (get_next_line(fd, &line) < 1)
+            break;
+        registerLine(anthill, line, index);
+        if (prevLine)
+            free(line);
+        index++;
     }
+    registerLine(anthill, line, index);
+    free(line);
 }
 
-int main(int ac, char **av) {
-    (void)ac;
-    (void)av;
-    
+int main() {
+
     t_data  anthill;
 
     readInput(&anthill, STDIN_FILENO);
-    system("leaks lem-in");
     printf("Done\n");
+    system("leaks lem-in");
 }
