c5e321f05bcb5b88d1087295baef3514fc79c699
Mehdi Taouil <mtaouil@c1r3p8.42nice.fr>
2022-07-04 16:27:56 +0200

    pas d'inspi c fini

--- src/paths.c
+++ src/paths.c
@@ -1,5 +1,17 @@
 #include "../includes/lem_in.h"
 
+int minCost(int cost[], size_t nbOfPath) {
+	int min = 0;
+	int minVal = cost[0];
+	for (size_t i = 1; i < nbOfPath; i++) {
+		if (minVal > cost[i]) {
+			min = i;
+			minVal = cost[i];
+		}
+	}
+	return min;
+}
+
 void letsFuckingGo(t_data *anthill, t_path *pathList, size_t nbOfPath) {
 	int *comb = ft_malloc(sizeof(int), nbOfPath);
 	int cost[nbOfPath];
@@ -13,14 +25,11 @@ void letsFuckingGo(t_data *anthill, t_path *pathList, size_t nbOfPath) {
 	}
 
 	while (ants) {
-		for (size_t i = 0; i < nbOfPath; i++) {
-			if (i == nbOfPath-1 || cost[i] <= cost[i+1]) {
-				comb[i]++;
-				cost[i]++;
-				ants--;
-				break ;
-			}
-		}
+		int min = minCost(cost, nbOfPath);
+		comb[min]++;
+		cost[min]++;
+		ants--;
 	}
 	printer(anthill, pathList, comb);
+	free(comb);
 }
